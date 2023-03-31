8a7bc60f105ccb04a1e05d5d4cea59360bbb5223
shiiiniii <115028625+shiiiniii@users.noreply.github.com>
2023-03-31 06:34:04 +0300

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,15 @@
+package finalwork;
+
+public class Square extends Rectangle{
+	public Square(double sideA) {
+		super(sideA, sideA);
+	}
+	
+	
+	public double getSideA() {
+		
+		return sideA;
+	}
+
+
+}
